0175e7121777a850a5c3b1a39af24cf919ab7422
Ken King <kking935@vt.ed>
2023-04-24 16:16:41 -0400

    improved interface

---
+++ components/About.tsx
@@ -0,0 +1,17 @@
+const About = () => {
+	return (
+		<div className='bg-gradient-to-b from-slate-100 to-slate-200 text-black p-10 rounded-xl shadow-lg mb-12'>
+			<h2 className='font-bold text-3xl text-center mb-3'>ABOUT EVA</h2>
+
+			<p className='text-md leading-7'>
+				<b>EVA</b> is an elderly virtual assistant leverages natural
+				language processing to identify health risks for socially
+				isolated senior citizens. By referencing to a senior
+				citizen&lsquo;s <b>EVA Health Report</b>, care teams can better
+				address risk factors and improve health outcomes.
+			</p>
+		</div>
+	);
+};
+
+export default About;
---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+	return (
+        // bg-dark
+		<footer className='bg-gradient-to-b from-slate-500 to-slate-700 text-center text-dark w-full px-10 pt-16 pb-28'>
+			<h1 className='text-lg font-extrabold'>EVA &copy; 2023</h1>
+		</footer>
+	);
+};
+
+export default Footer;
