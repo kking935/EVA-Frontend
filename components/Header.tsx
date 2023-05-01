1f0297bee0dcb57ba6880c2d1eaac251ae9d3a12
Ken King <kking935@vt.ed>
2023-04-30 23:29:01 -0400

    added new logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,23 @@
 import Link from 'next/link';
-import { FaHandHoldingHeart } from 'react-icons/fa';
 import Image from 'next/image';
+// import { FaHandHoldingHeart } from 'react-icons/fa';
 
 const Header = () => {
 	return (
 		// bg-primary
-		<header className='bg-gradient-to-b from-blue-300 to-blue-400 w-full px-1 sm:px-10 py-4 flex items-center justify-between flex-col space-y-3 sm:space-y-0 md:flex-row flex-wrap md:flex-nowrap'>
+		<header className='bg-gradient-to-b from-blue-300 to-blue-400 w-full px-1 md:px-10 py-4 flex items-center justify-between flex-col space-y-2 md:space-y-0 md:flex-row flex-wrap md:flex-nowrap'>
 			<Link
-				className='flex flex-row justify-center items-center space-x-4'
+				className='flex flex-row justify-center items-center space-x-4 p-3 rounded-xl'
 				href='/'
 			>
-				{/* <FaHandHoldingHeart size={32} />
-				<h1 className='text-2xl font-extrabold'>EVA</h1> */}
-				<Image src='/images/eva.png' alt='eva' width={250} height={250} />
+				<Image
+					src='/images/eva.png'
+					alt='eva'
+					width={250}
+					height={250}
+				/>
 			</Link>
-			<nav className='flex space-x-3 sm:space-x-8 flex-row flex-wrap md:flex-nowrap'>
+			<nav className='md:border-none border-t pt-5 border-black flex space-x-3 md:space-x-8 flex-row flex-wrap md:flex-nowrap'>
 				<Link
 					className='font-semibold transition transform duration-200 ease-in hover:bg-gray-200 p-3 rounded-md'
 					href={'/about/risks'}
